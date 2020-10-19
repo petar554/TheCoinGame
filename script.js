25a574012a62a2b6f189a6caed5293bde4ba0e04
petarPAN <petarburic554@gmail.com>
2020-10-19 17:09:46 +0200

    Add function that responds  when an avatar and coin overlap

---
+++ script.js
@@ -0,0 +1,13 @@
+function isTouching(a, b) {
+    const aRect = a.getBoundingClientRect();
+    const bRect = b.getBoundingClientRect();
+
+    return !(
+        aRect.top + aRect.height < bRect.top ||
+        aRect.top > bRect.top + bRect.height ||
+        aRect.left + aRect.width < bRect.left ||
+        aRect.left > bRect.left + bRect.width
+    );
+}
+
+Add function whick make reaction when avatar and coin are overllaped
