93e684f3409da28e4789b9272b892c071b06a782
petarPAN <petarburic554@gmail.com>
2020-10-21 18:47:05 +0200

    Add event listener for avatar moving

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ function isTouching(a, b) {
     );
 }
 
+const avatar = document.querySelector('#avatar');
+const coin = document.querySelector('#coin');
+const result = document.querySelector('#result');
+const btn = document.querySelector('#btn');
+btn.style.visibility = 'hidden';
+
+window.addEventListener('keyup', function (e) {
+    if (e.key === 'ArrowUp' || e.key === 'Up') {
+        arrowUpDown(avatar, -50);
+    }
+    else if (e.key === 'ArrowDown' || e.key === 'Down') {
+        arrowUpDown(avatar, 50);
+    }
+    else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+        arrowLeftRight(avatar, -50);
+    }
+    else if (e.key === 'ArrowRight' || e.key === 'Right') {
+        arrowLeftRight(avatar, 50);
+    }
+
+});
+
+const arrowUpDown = (avatar, amount) => {
+    const currValue = extractNum(avatar.style.top);
+    avatar.style.top = `${currValue + amount}px`
+}
+const arrowLeftRight = (avatar, amount) => {
+    const currValue = extractNum(avatar.style.left);
+    avatar.style.left = `${currValue + amount}px`
+}
+
+const extractNum = (string) => {
+    if (!string) return 200;
+    return parseInt(string.slice(0, -2));
+}
